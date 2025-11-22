e81244241cadb1324d52be79f15cc099a6e24acf
illumeow <illumeow0223@gmail.com>
2025-11-22 16:56:45 +0800

    ALL -> all

--- codebook/3_Data_Structure/discrete_trick.cpp
+++ codebook/3_Data_Structure/discrete_trick.cpp
@@ -1,9 +1,9 @@
 vector<int> val;
 // build
-sort(ALL(val)), val.resize(unique(ALL(val)) - val.begin());
+sort(all(val)), val.resize(unique(all(val)) - val.begin());
 // index of x
-upper_bound(ALL(val), x) - val.begin();
+upper_bound(all(val), x) - val.begin();
 // max idx <= x
-upper_bound(ALL(val), x) - val.begin();
+upper_bound(all(val), x) - val.begin();
 // max idx < x
-lower_bound(ALL(val), x) - val.begin();
+lower_bound(all(val), x) - val.begin();
