f62407133761bd610acaa405b089ccbe805e194b
illumeow <illumeow0223@gmail.com>
2025-11-22 17:38:12 +0800

    rename

--- codebook/3_Data_Structure/Segment_Tree.cpp
+++ codebook/3_Data_Structure/Segment_Tree.cpp
@@ -1,21 +1,21 @@
-struct Segment_Tree { //懶標動態開點
+struct Segment_Tree {  // dynamic
   struct node {
-    int data, lazy;
+    int data, tag;
     node *l, *r;
-    node() : data(0), lazy(0), l(0), r(0) {}
+    node() : data(0), tag(0), l(0), r(0) {}
     void up() {
       if (l) data = max(l->data, r->data);
     }
     void down() {
       if (l) {
-        l->data += lazy, l->lazy += lazy;
-        r->data += lazy, r->lazy += lazy;
+        l->data += tag, l->tag += tag;
+        r->data += tag, r->tag += tag;
       }
-      lazy = 0;
+      tag = 0;
     }
   } * root;
-  int l, r;
-  node *build(int l, int r, int *data) {
+  int L, R;
+  node *build(int l, int r, vector<int> &data) {
     node *p = new node();
     if (l == r) return p->data = data[l], p;
     int m = (l + r) / 2;
@@ -23,34 +23,34 @@ struct Segment_Tree { //懶標動態開點
     p->r = build(m + 1, r, data);
     return p->up(), p;
   }
-  void s_modify(
-    int L, int R, int l, int r, node *p, int x) {
-    if (r < L || l > R) return;
+  void _modify(  // [l, r] += x
+    int l, int r, int L, int R, node *p, int x) {
+    if (r < L || R < l) return;
     p->down();
     if (L <= l && R >= r)
-      return p->data += x, p->lazy += x, void();
+      return p->data += x, p->tag += x, void();
     int m = (l + r) / 2;
-    s_modify(L, R, l, m, p->l, x);
-    s_modify(L, R, m + 1, r, p->r, x);
+    _modify(L, R, l, m, p->l, x);
+    _modify(L, R, m + 1, r, p->r, x);
     p->up();
   }
-  int s_query(int L, int R, int l, int r, node *p) {
+  int _query(int l, int r, int L, int R, node *p) {
     p->down();
-    if (L <= l && R >= r) return p->data;
-    int m = (l + r) / 2;
-    if (R <= m) return s_query(L, R, l, m, p->l);
-    if (L > m) return s_query(L, R, m + 1, r, p->r);
-    return max(s_query(L, R, l, m, p->l),
-      s_query(L, R, m + 1, r, p->r));
+    if (l <= L && R <= r) return p->data;
+    int M = (L + R) / 2;
+    if (r <= M) return _query(l, r, L, M, p->l);
+    if (L > M) return _query(l, r, M + 1, R, p->r);
+    return max(_query(l, r, L, M, p->l),
+               _query(l, r, M + 1, R, p->r));
   }
-  void init(int L, int R, int *data) {
-    l = L, r = R;
+  void init(int l, int r, vector<int> &data) {
+    L = l, R = r;
     root = build(l, r, data);
   }
-  void modify(int L, int R, int x) {
-    s_modify(L, R, l, r, root, x);
+  void modify(int l, int r, int x) {
+    _modify(l, r, L, R, root, x);
   }
-  int query(int L, int R) {
-    return s_query(L, R, l, r, root);
+  int query(int l, int r) {
+    return _query(l, r, L, R, root);
   }
 };
