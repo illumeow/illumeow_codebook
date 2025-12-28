2aecf959ad96bf077fe3dd6c9c22c0b32958702b
illumeow <illumeow0223@gmail.com>
2025-12-29 01:03:13 +0800

    manacher

--- codebook/5_String/Manacher.cpp
+++ codebook/5_String/Manacher.cpp
@@ -1,15 +1,17 @@
-int z[N]; // 0-base
+// 0-base
 /* center i: radius z[i * 2 + 1] / 2
-   center i, i + 1: radius z[i * 2 + 2] / 2
+   center i - 1, i: radius z[i * 2] / 2
    both aba, abba have radius 2 */
-void Manacher(string tmp) {
+vector<int> Manacher(string &tmp) {
   string s = "%";
-  int l = 0, r = 0;
+  int m = 0, r = 0;
   for (char c : tmp) s.push_back(c), s.push_back('%');
+  vector<int> z(s.size());
   for (int i = 0; i < s.size(); ++i) {
-    z[i] = r > i ? min(z[2 * l - i], r - i) : 1;
+    z[i] = r > i ? min(z[2 * m - i], r - i) : 1;
     while (i - z[i] >= 0 && i + z[i] < s.size() 
            && s[i + z[i]] == s[i - z[i]]) ++z[i];
-    if (z[i] + i > r) r = z[i] + i, l = i;
+    if (z[i] + i > r) r = z[i] + i, m = i;
   }
+  return z;
 }
